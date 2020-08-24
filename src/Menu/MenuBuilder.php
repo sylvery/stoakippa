b7b1c451fc9f5afdbaceb4b5eb440b2fdbabba4c
sylvery <silvery4543@gmail.com>
2020-08-25 06:49:31 +1000

    final changes and ready to push to github

---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+
+class MenuBuilder
+{
+    private $factory;
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMenu(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->addChild('Home', ['route' => 'home']);
+        $menu->addChild('Sales', ['route' => 'sale_index']);
+        $menu->addChild('transactions', ['route' => 'transaction_index']);
+        $menu->addChild('stocks', ['route' => 'stock_index']);
+        $menu->addChild('items', ['route' => 'item_index']);
+        $menu->addChild('manufacturers', ['route' => 'item_manufacturer_index']);
+        return $menu;
+    }
+}
